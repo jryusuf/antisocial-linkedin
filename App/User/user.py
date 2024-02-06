fe914da4ea403be1a086bb5ab42dea1763307c3f
Yusuf Yuksel <yusufyuksel@aol.com>
2024-02-06 08:14:51 +0000

    Remove email field uniqueness constraint

--- App/User/user.py
+++ App/User/user.py
@@ -1,10 +1,9 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
-from pydantic import EmailStr
 from datetime import datetime
 
 class UserBase(SQLModel):
-    email: EmailStr = Field(unique=True)
+    #email: EmailStr = Field(unique=True)
     password: str
     creadted_at: datetime = Field(default=datetime.now)
     updated_at: datetime = Field(default=datetime.now)
