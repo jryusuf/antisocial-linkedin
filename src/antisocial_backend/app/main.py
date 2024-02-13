3cf5c84d138a2f5bd5130f3d7253532c0007230c
Yusuf Yuksel <yusufyuksel@aol.com>
2024-02-13 14:25:44 +0000

    Add FastAPI routes and endpoint

--- src/antisocial_backend/app/main.py
+++ src/antisocial_backend/app/main.py
@@ -1,6 +1,13 @@
-def main():
-    # Your code here
+from fastapi import FastAPI, Depends
 
-    if __name__ == "__main__":
-        print("Hello, World!")
-        main()
+from antisocial_backend.dependencies.dependencies import get_query_token
+from antisocial_backend.routes import users
+
+app = FastAPI()
+
+app.include_router(router=users.router)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
