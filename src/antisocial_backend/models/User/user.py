c194601cff0158ecd2470ae67304fbc467bba7a6
Yusuf Yuksel <yusufyuksel@aol.com>
2024-02-13 15:14:26 +0000

    Refactor User model to use UserBase class***

--- src/antisocial_backend/models/User/user.py
+++ src/antisocial_backend/models/User/user.py
@@ -3,6 +3,12 @@ from sqlmodel import Field, SQLModel, AutoString
 from datetime import datetime
 from pydantic import EmailStr
 
-class UserCreate(SQLModel, table=True):
-    email_address: str = Field(primary_key=True)
+class UserBase(SQLModel):
+    pass
+class User(UserBase, table=True):
+    email_address: EmailStr = Field(primary_key=True,sa_type=AutoString)
+    password: str = ...
+
+class UserCreate(UserBase):
+    email_address: EmailStr = Field(primary_key=True,sa_type=AutoString)
     password: str = ...
