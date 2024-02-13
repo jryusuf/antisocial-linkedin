c72d1042e8efaeece147b12ac4675add3d922376
Yusuf Yuksel <yusufyuksel@aol.com>
2024-02-13 20:29:50 +0000

    Refactor user routes in users.py

--- src/antisocial_backend/routes/users.py
+++ src/antisocial_backend/routes/users.py
@@ -12,41 +12,28 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=list[UserRead])
-async def read_users(
-    *,
-    session:Session = Depends(get_session)
-):
+async def read_users(*,session:Session = Depends(get_session)):
     users = session.exec(select(User)).all()
     if not users:
         raise HTTPException(status_code=404, detail="Users not found")
     return users
 
 @router.get("/{user_id}" , response_model=UserRead)
-async def read_user(
-    *,
-    session:Session = Depends(get_session),
-    user_id: int):
+async def read_user(*,session:Session = Depends(get_session),user_id: int):
     user = session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
     
-
 @router.post("/", response_model=UserRead)
-async def create_user(
-    *, 
-    session:Session = Depends(get_session),
-    user: UserCreate):
+async def create_user(*, session:Session= Depends(get_session),user: UserCreate):
     try:
         return create_user_db(session=session,user=user)
     except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=422, detail=str(e))
 
 @router.put("/{user_id}", response_model=UserRead)
-async def update_user(
-    *,
-    session:Session = Depends(get_session),
-    user_id: int, user: UserUpdate):
+async def update_user(*,session:Session=Depends(get_session),user_id: int, user: UserUpdate):
     db_user = session.get(User, user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -59,10 +46,7 @@ async def update_user(
     return db_user
 
 @router.delete("/{user_id}")
-async def delete_user(
-    *,
-    session:Session = Depends(get_session),
-    user_id: int):
+async def delete_user(*,session:Session=Depends(get_session),user_id: int):
     user = session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
