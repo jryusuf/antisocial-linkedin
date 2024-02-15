42bf58ae5c70dc13dce3ba88bf9f6c7837dd1bc2
Yusuf Yuksel <yusufyuksel@aol.com>
2024-02-15 13:19:24 +0000

    Add Person CRUD operations

--- src/antisocial_backend/models/Person/__init__.py
+++ src/antisocial_backend/models/Person/__init__.py
@@ -1,1 +1,7 @@
+
+from .person import PersonCreate
+from .person import PersonRead
+from .person import PersonUpdate
+from .person import PersonDelete
+from .person import PersonBase
 from .person import Person
